ba956274ad7d3bfe1151dae4bced14f5624fd332
Yujian Li <jeylee2018@gmail.com>
2020-11-15 21:19:42 +0000

    Test the mount of  frontend ticketlist componnet

--- zoa2021/__test__/ticketlist.test.js
+++ zoa2021/__test__/ticketlist.test.js
@@ -1,9 +1,22 @@
 import { mount } from '@vue/test-utils';
 import TicketList from './../src/components/TicketList.vue';
+import Vue from 'vue';
+import BoostrapVue from 'bootstrap-vue';
 
-describe('TicketList', () => {
-  // Inspect the raw component options
+Vue.use(BoostrapVue);
+
+describe('Mounted TicketList', () => {
+  const wrapper = mount(TicketList);
+  test('is a Vue instance', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+
   it('has data', () => {
     expect(typeof TicketList.data).toBe('function');
+  });
+
+  it('renders correctly with different data', async () => {
+    await wrapper.vm.$nextTick();
+    expect(wrapper.text()).toContain('Updated At');
   });
 });
